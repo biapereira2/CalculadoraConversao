49bc1e5e1b7b420054141187e305ac74da5a407b
biapereira2 <bcbsp@cesar.school>
2024-08-23 12:10:22 -0300

    fix: mostra na tela os passos da conversão de decimal para binário

--- calculadora.c
+++ calculadora.c
@@ -1,13 +1,16 @@
 // Beatriz Carla Bandeira da Silva Pereira - 3°A - 2024.2
 // Implementação da função de conversão de decimal para binário (1A) às 11h47 do dia 23/08/2024
+// Ajuste na funçao decimalParaBin para mostrar na tela os passos para conversão de decimal para binário às 12h09 do dia 23/08/2024
 
 #include <stdio.h>
 #include <stdlib.h>
 
 void decimalParaBin (int val, int bin[64]){
-    int i = 0;
+    int i = 0, resto;
     while (val > 0){
-        bin[i] = val % 2;
+        resto = val % 2;
+        bin[i] = resto;
+        printf(" %d / 2 = %d\n", val, resto);
         val = val / 2;
         i++;
     }
